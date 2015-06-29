f7adde7ff11715a0ed781e96c14e7ffd5e468913
Mihai Scurtu <mscurtu90@gmail.com>
2015-06-29 11:32:25 +0300

    Capitalize variable name in main test file

--- main.go
+++ main.go
@@ -7,13 +7,13 @@ import (
 )
 
 func main() {
-	reddit := reddit.NewClient("Testing Go Wrapper")
+	Reddit := reddit.NewClient("Testing Go Wrapper")
 
-	fmt.Println(reddit.UserAgent)
+	fmt.Println(Reddit.UserAgent)
 
 	// fmt.Println(reddit.Get("/?limit=1"))
 
-	postList := reddit.GetFrontPage()
+	postList := Reddit.GetFrontPage()
 
 	for _, p := range postList.GetChildren() {
 		fmt.Println(p.Title)
