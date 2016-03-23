b9daf1fbbc8fb987e6566202eabd6487de3766ce
Mihai Scurtu <mscurtu90@gmail.com>
2016-03-23 10:41:43 +0200

    Play with anagrams

--- main.go
+++ main.go
@@ -2,20 +2,58 @@ package main
 
 import (
 	"fmt"
-
 	"github.com/mihai-scurtu/reddit-go/reddit"
+	"sort"
+	"strings"
+	"time"
 )
+
+type sortRunes []rune
+
+func (s sortRunes) Less(i, j int) bool {
+	return s[i] < s[j]
+}
+
+func (s sortRunes) Swap(i, j int) {
+	s[i], s[j] = s[j], s[i]
+}
+
+func (s sortRunes) Len() int {
+	return len(s)
+}
+
+func SortString(s string) string {
+	r := []rune(s)
+	sort.Sort(sortRunes(r))
+	return string(r)
+}
+
+func anagramHash(s string) string {
+	return SortString(strings.Replace(s, " ", "", -1))
+}
 
 func main() {
 	Reddit := reddit.NewClient("Testing Go Wrapper")
 
+	var anagrams = make(map[string]string)
+
 	fmt.Println(Reddit.UserAgent)
 
-	// fmt.Println(reddit.Get("/?limit=1"))
+	for {
+		postList := Reddit.GetNewPosts()
 
-	postList := Reddit.GetFrontPage()
+		for _, p := range postList.GetChildren() {
+			hash := anagramHash(p.Title)
+			match := anagrams[hash]
 
-	for _, p := range postList.GetChildren() {
-		fmt.Println(p.Title)
+			if len(match) > 0 && match != p.Title {
+				fmt.Println("!!! FOUND !!! '" + p.Title + "' = '" + match + "'")
+			}
+
+			match = p.Title
+		}
+
+		fmt.Println(".")
+		time.Sleep(1 * time.Minute)
 	}
 }
